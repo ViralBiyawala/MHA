044e6aee9f1cfe7379d3a328887742fc7211c617
ViralBiyawala <ui21cs66@iiitsurat.ac.in>
2024-05-21 16:21:24 +0530

    Mail added

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, request, jsonify
 import neattext.functions as nfx
 import joblib
 import numpy as np
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 
 app = Flask(__name__)
 
@@ -22,6 +26,43 @@ def clean_text(text):
     # Additional preprocessing steps can be added here
     return cleaned_text
 
+
+def send_email(user_details):
+    # Email configuration
+    sender_email = "lifesaver102023@gmail.com"
+    recipient_list = ['svspbs567@gmail.com']
+
+    # Create message container - the correct MIME type is multipart/alternative
+    msg = MIMEMultipart()
+    msg['Subject'] = f"Urgent Concern for {user_details['name']}'s Well-being"
+    msg['From'] = sender_email
+    msg['To'] = ", ".join(recipient_list)
+
+    # Create the body of the message
+    message = f'''Dear Mental Health Support Foundation Team,
+    
+We have identified a post on our platform that raises serious concerns about the user's mental health and safety. We are reaching out to seek your immediate intervention and support.
+
+User Details:
+Name: {user_details['name']}
+Mobile No.: {user_details['mobile_number']}
+Email: {user_details['email']}
+
+Given the gravity of the situation, we kindly request your assistance in connecting with the user to ensure their safety and well-being. Your expertise and resources could be invaluable in providing the necessary support and guidance during this critical time.
+
+Thank you for your prompt attention to this matter.
+
+Sincerely,
+MindCare Support Team'''
+
+    # Attach the message to the email
+    msg.attach(MIMEText(message, 'plain'))
+
+    # Send the email
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+        server.login(sender_email, 'aouojaqtpwhlzoiy')
+        server.sendmail(sender_email, recipient_list, msg.as_string())
+
 @app.route('/api/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
@@ -40,6 +81,15 @@ def predict():
         # Convert numpy arrays to lists and ensure they are serializable
         predictions = [float(pred) for pred in predictions]
         predictions_score = [float(score) for score in predictions_score]
+        
+        if predictions[0] > 0.9:
+            
+            user_details = {
+                'name': data.get('user_name', []),  # Replace with user['user_name']
+                'mobile_number': data.get('mobile_number', []),  # Replace with user['mobile_no']
+                'email': data.get('email', [])  # Replace with email
+            }
+            send_email(user_details)
 
         return jsonify({"predicted_labels": predictions, "predicted_scores": predictions_score})
 
